607c81be871b4b947c1846b07eedcc7c5608700e
Nishant Singh <nraj14908@gmail.com>
2025-10-08 04:54:02 +0530

    dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 
 export default function dashboard() {
-    return <div>
+    return <div className="text-center py-10 bg-white border-b border-gray-100">
+    <h1 className="text-4xl font-bold text-gray-800 tracking-tight">
         DashBoard
-    </div>
+    </h1>
+    <p className="mt-2 text-base text-indigo-600 font-semibold">
+        Under Process: Stay Tuned
+    </p>
+</div>
 }
